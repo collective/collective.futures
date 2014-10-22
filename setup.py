9007b6bc318931ce95100ac655f8c93b86262eed
Asko Soukka <asko.soukka@iki.fi>
2014-10-23 01:38:22 +0300

    Back to development: 0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.futures',
-    version='0.9.0',
+    version='0.9.1.dev0',
     description='',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
