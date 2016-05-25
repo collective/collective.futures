9a512fb82fd7c385414596c9ea34cb7dc2775b73
Asko Soukka <asko.soukka@iki.fi>
2016-05-25 15:40:54 +0300

    Preparing release 0.9.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.9.1 (unreleased)
+0.9.1 (2016-05-25)
 ------------------
 
 - Fix issue where resolved futures were not passed for nested requests
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.futures',
-    version='0.9.1.dev0',
+    version='0.9.1',
     description='Experimental non-blocking futures for Plone',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
